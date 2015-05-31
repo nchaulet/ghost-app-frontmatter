cccead1961a888f5467888bca598a97fe45256e4
Nicolas Chaulet <n.chaulet@gmail.com>
2015-05-31 12:29:04 +0200

    refacto

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ var App       = require('ghost-app'),
     converter = new Showdown.converter({extensions: ['ghostgfm', 'footnotes', 'highlight']}),
     matter    = require('gray-matter');
 
+var parseFrontMatter = function(post) {
+    post.markdown = post.markdown.replace(/^\n/, '');
+    var parsed = matter(post.markdown, {
+        delims: ['+++', '+++']
+    });
+    post.markdown = parsed.content;
+    post.html = converter.makeHtml(post.markdown);
+    post.data = parsed.data;
+};
+
+var cleanDescription = function(description) {
+    return description.replace(/\+\+\+[\S\s]*\+\+\+/m, '');
+};
+
 var MyApp = App.extend({
     install: function () {},
     uninstall: function () {},
 
     activate: function () {
-
-        var parseFrontMatter = function(post) {
-            post.markdown = post.markdown.replace(/^\n/, '');
-            var parsed = matter(post.markdown, {
-                delims: ['+++', '+++']
-            });
-            post.markdown = parsed.content;
-            post.html = converter.makeHtml(post.markdown);
-            post.data = parsed.data;
-        };
-
         this.ghost.filters.register('prePostsRender', function (posts) {
-
             if (posts instanceof Array) {
                 posts.forEach(function(post) {
                     parseFrontMatter(post);
@@ -33,10 +35,6 @@ var MyApp = App.extend({
         });
 
         this.ghost.filters.register('rss.feed', function(feed) {
-
-            var cleanDescription = function(description) {
-                return description.replace(/\+\+\+[\S\s]*\+\+\+/m, '');
-            };
 
             feed.items.forEach(function(item) {
                 var contentEncoded    = item.custom_elements[0]['content:encoded'];
